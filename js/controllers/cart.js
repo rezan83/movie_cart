4beabd636301219733bb2833e172d8fef51e54bb
rezan83 <rezan.moh83@gmail.com>
2018-11-20 23:51:09 +0100

    item get added only once

--- js/controllers/cart.js
+++ js/controllers/cart.js
@@ -2,12 +2,17 @@ app.controller('cart', ['$scope', '$rootScope', function($scope, $rootScope) {
 
 	$scope.cart = $rootScope.cart;
 
-	$rootScope.removeCart = function(array, index) {
-	// remove cart item from dom
+	$rootScope.removeCart = function(array, index, item) {
+
+		// renabling adding bottun in the home main list
+		$rootScope.items[$rootScope.items.indexOf(item)].noAdd = false;
+
+		// remove cart item from dom
 		$rootScope.cart[index].showElement = false;
-	// remove item from global cart array
+		// remove item from global cart array and from total
 		array.splice(index, 1);
-		
+		$rootScope.total -= 1;
 	}
 
 }]);
+
--- js/controllers/itemList.js
+++ js/controllers/itemList.js
@@ -1,17 +1,22 @@
 app.controller('itemList', ['$scope', '$rootScope', 'items', function($scope, $rootScope, items) {
 
 	items.success(function(data) {
-		$scope.items = data.Search;
+		$rootScope.items = data.Search;
 
 	});
 
 	// global cart array
 	$rootScope.cart = [];
+	$rootScope.total = 0;
 
 	// add item to cart
-	$rootScope.addToCart = function(item) {
+	$rootScope.addToCart = function(item, index) {
 
+		// disabling adding bottun so item get added only once
+		$rootScope.items[index].noAdd = true;
+		// adding to cart
 		$rootScope.cart.push(item);
+		$rootScope.total += 1;
 
 	}
 
